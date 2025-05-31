ed2d2bd1a44150a154690c0fcfaba38880d222c1
WesleyLuiz21 <wesley_luiz2001@hotmail.com>
2025-05-31 09:54:43 +0100

    created server config

--- app.js
+++ app.js
@@ -9,3 +9,13 @@ app.use(express.urlencoded({ extended: true }));
 // EJS config
 app.set('view engine', 'ejs');
 app.set('view', path.join(__dirname, 'views'));
+
+// Routes Setup
+const routes = require("./routes/index");
+app.use('/', routes);
+
+//Server Setup
+const PORT = 5000;
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
