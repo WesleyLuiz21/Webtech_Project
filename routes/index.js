452ceded2e3d07bfdbc0e0190589e82a4bc53dfd
WesleyLuiz21 <wesley_luiz2001@hotmail.com>
2025-05-31 14:58:32 +0100

    created all pages and created the routing logic

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,29 @@
 const express = require('express');
 const router = express.Router();
 
+// Pages logic to render to our website
 router.get('/', (req, res) => {
     res.render('pages/home');   
+});
+
+router.get('/courses', (req,res) => {
+    res.render('pages/courses');
+});
+
+router.get('/events', (req,res) => {
+    res.render('pages/events');
+});
+
+router.get('/about', (req,res) => {
+    res.render('pages/about');
+});
+
+router.get('/contact', (req,res) => {
+    res.render('pages/contact');
+});
+
+router.get('/faq', (req,res) => {
+    res.render('pages/faq');
 });
 
 module.exports = router;
